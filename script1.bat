cacbf1acea7c4b28f1157d1d31666dee037d0ffa
akarshiverma <82092032+akarshiverma@users.noreply.github.com>
2022-12-16 18:30:11 +0530

    Windows script

---
+++ script1.bat
@@ -0,0 +1,22 @@
+cd c:\ 
+mkdir Powershell
+cd PowerShell
+mkdir dirl
+mkdir dir2
+cd dirl
+mkdir diril 
+mkdir dir12
+cd dir11
+
+type nul >>filel.txt 
+copy nul file2.txt
+type nul >>>filel.csv 
+copy nul file2.csv
+
+cd c:\PowerShell\dir2 mkdir dir21 
+mkdir dir22
+
+cd dir21 
+mkdir dir212
+
+dir c:\Powershell/s
